147fb19274d4e6b166b34c235ee575649ddf8747
Erica Levinsky <elevinsky@gmail.com>
2016-11-01 19:36:06 -0500

    finished actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,7 +4,7 @@ class ActorsController < ApplicationController
   end
 
   def show
-    @actor = Actor.find(params[:id])
+    @actor = Actor.find_by({ :id => params[:id] })
   end
 
   def new_form
@@ -23,16 +23,17 @@ class ActorsController < ApplicationController
   end
 
   def edit_form
-    @actor = Actor.find(params[:id])
+    @actor = Actor.find_by({ :id => params[:id] })
   end
 
   def update_row
-    @actor = Actor.find(params[:id])
+    @actor = Actor.find_by({ :id => params[:id] })
 
     @actor.dob = params[:dob]
     @actor.name = params[:name]
     @actor.bio = params[:bio]
     @actor.image_url = params[:image_url]
+    @actor.id = params[:id]
 
     @actor.save
 
@@ -40,7 +41,7 @@ class ActorsController < ApplicationController
   end
 
   def destroy
-    @actor = Actor.find(params[:id])
+    @actor = Actor.find_by({ :id => params[:id] })
 
     @actor.destroy
   end
